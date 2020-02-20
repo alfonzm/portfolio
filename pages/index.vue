6ce483ed6f16a4248549956ad78e9df8e0f9ab7b
Alfonz <m.alfonz@gmail.com>
2020-02-21 00:33:33 +0800

    Fix animation timing

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
 
           //- nav links
           ul.flex.mt-6
-            li(v-for="(link, i) in data.links" data-aos="fade-up" :data-aos-delay="String((i+1) * 300)")
+            li(v-for="(link, i) in data.links" data-aos="fade-up" :data-aos-delay="String((i+1) * 200)")
               a.big-link.animated-link.capitalize.text-xl.sm_text-3xl.mr-4.sm_mr-12(href='#' v-scroll-to="`#${link}`") {{ link }}
 
       //- Projects
@@ -20,7 +20,7 @@
         .max-w-4xl
           h2.text-3xl.sm_text-4xl.font-bold.leading-tight(data-aos="fade-up") Here are some projects I worked on.
 
-        section.mt-12(v-for="project in data.projects" data-aos="fade-up")
+        section.mt-12(v-for="project in data.projects" data-aos="fade-right")
           h3.text-3xl {{ project.title }}
           p
             template(v-for="(link, index) in project.links")
@@ -29,14 +29,14 @@
                 |
                 | /
                 |
-          .my-4(data-aos="fade-up")
+          .my-4
             p.text-gray-700(v-html="project.description")
 
-          div(data-aos="fade-up")
+          div
             div.p-4.sm_p-16.bg-gray-100
               img.w-full(:src="require(`@/assets/images/${project.image_url}`)")
 
-        section.mt-12(data-aos="fade-up")
+        section.mt-12(data-aos="fade-right")
           .text-md.sm_text-xl.text-gray-400
             | ...and
             |
@@ -51,16 +51,16 @@
 
         section
           .flex.flex-wrap.-mx-4
-            div.px-4.mt-12(v-for="game in data.games" class="w-full lg_w-1/2")
-              h3.text-3xl(data-aos="fade-up") {{ game.title }}
-              p.mb-4(data-aos="fade-up")
+            div.px-4.mt-12(v-for="game in data.games" class="w-full lg_w-1/2" data-aos="fade-right")
+              h3.text-3xl {{ game.title }}
+              p.mb-4
                 template(v-for="(link, index) in game.links")
                   a.animated-link.small-link(:href="link.url" target="_blank").text-teal-400 {{ link.label }}
                   template(v-if="index < game.links.length - 1")
                     |
                     | /
                     |
-              section.flex.bg-gray-100.p-2.sm_p-4.game-image-wrapper(data-aos="fade-up")
+              section.flex.bg-gray-100.p-2.sm_p-4.game-image-wrapper
                 .m-auto
                   img.shadow-xl(:src="require(`@/assets/images/${game.image_url}`)")
 
@@ -70,7 +70,7 @@
           h2.text-3xl.sm_text-4xl.font-bold.leading-tight(data-aos="fade-up") I do talks & workshops about web and game development as well.
 
         //- Nuxt talk
-        section.mt-10(v-for="talk in data.talks" data-aos="fade-up")
+        section.mt-10(v-for="talk in data.talks" data-aos="fade-right")
           h3.text-2xl {{ talk.title }}
           .mb-4
             p.text-gray-600 {{ talk.description }}
@@ -79,29 +79,29 @@
 
       //- Work
       section.pt-16.mt-32#about
-        h2.text-3xl.sm_text-4xl.font-bold.leading-tight.anchor-work(data-aos="fade-up") I've been working in the IT industry for almost six years.
+        h2.text-3xl.sm_text-4xl.font-bold.leading-tight(data-aos="fade-up") I've been working in the IT industry for almost six years.
 
-        section.companies-section.mt-10
+        section.companies-section.mt-10.anchor-work(data-aos-offset="150")
           .company(
             v-for="(company, i) in data.companies"
             :class="{ 'pb-10': i < data.companies.length-1 }"
           )
-            h3.text-xl.sm_text-2xl.leading-tight(data-aos="fade-right" data-aos-anchor=".anchor-work" :data-aos-delay="String((i*300)+100)") {{ company.position }}
-            p.text-gray-600(data-aos="fade-right" data-aos-anchor=".anchor-work" :data-aos-delay="String((i*300)+200)") {{ company.name }}
-            p.text-gray-600(data-aos="fade-right" data-aos-anchor=".anchor-work" :data-aos-delay="String((i*300)+300)") {{ company.period }}
+            h3.text-xl.sm_text-2xl.leading-tight(data-aos="fade-right" data-aos-offset="150" data-aos-anchor=".anchor-work" :data-aos-delay="String((i*300)+100)") {{ company.position }}
+            p.text-gray-600(data-aos="fade-right" data-aos-offset="150" data-aos-anchor=".anchor-work" :data-aos-delay="String((i*300)+200)") {{ company.name }}
+            p.text-gray-600(data-aos="fade-right" data-aos-offset="150" data-aos-anchor=".anchor-work" :data-aos-delay="String((i*300)+300)") {{ company.period }}
 
       //- Skills/Tools
       section.mt-40
         h2.text-3xl.sm_text-4xl.font-bold.leading-tight(data-aos="fade-up") Here's a list of my skillset and the tools I use.
 
         template(v-for="(skills, skillCategoryIdx) in data.skills")
-          h3.text-2xl.mt-8(data-aos="fade-up" data-aos-offset="150" :class="`anchor-skill-${skillCategoryIdx}`") {{ skills.name }}
+          h3.text-2xl.mt-8(data-aos="fade-right" data-aos-offset="150" :class="`anchor-skill-${skillCategoryIdx}`") {{ skills.name }}
           ul.text-gray-600.leading-6
             li(
               v-for="(skill, subSkillIdx) in skills.skills"
               data-aos="fade-right"
               data-aos-offset="150"
-              :data-aos-delay="String(100 + (subSkillIdx * 50))"
+              :data-aos-delay="String((subSkillIdx+1) * 150)"
               :data-aos-anchor="`.anchor-skill-${skillCategoryIdx}`"
             ) {{ skill.join(', ') }}
 
